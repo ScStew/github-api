ca7f9056eba24eef43b05d1676b3d71b085f9966
ScStew <scottmstewart2@gmail.com>
2017-12-14 15:59:18 -0500

    added date input to login

--- app.rb
+++ app.rb
@@ -11,14 +11,16 @@ end
 post "/after_login" do
     session[:username] = params[:username]
     session[:pass] = params[:pass]
+    session[:date] =  params[:date]
     redirect "/next_page"
 end
 
 get "/next_page" do
     user = session[:username]
     pass = session[:pass]
+    date = session[:date]
     git_class = Git_api_class.new(user, pass)
-    data = git_class.get_api_data("2017-12-10")
+    data = git_class.get_api_data("#{date}")
     # p data
    erb :next_page, locals: {data:data}
 end
